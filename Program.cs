7883dfb9fe17fc914079d5f9c2ff9af9ff12c325
20100 <mmmelkady23@gmail.com>
2024-12-20 22:30:06 +0200

    Answer 2

--- Program.cs
+++ Program.cs
@@ -5,22 +5,53 @@
         static void Main(string[] args)
         {
             #region Q19:Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+            //Console.WriteLine("enter size of Array");
+            //int n;
+            //while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+            //{
+            //    Console.Write("enter size of Array  integer & positive");
+            //}
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (i == j) Console.Write("1 ");
+            //        else Console.Write("0 ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region Q20:Write a program in C# Sharp to find the sum of all elements of the array.
+            int[] Arr = [10,20,30,32,41,41,352,1,345,346];
+
+            int sum = 0,sum1=0;
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                sum += Arr[i];
+            }
+            Console.WriteLine($"the sum of Array is : {sum}");
+
+            // Another solve 
             Console.WriteLine("enter size of Array");
             int n;
+            while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+            {
+                Console.Write("enter size of array  integer & positive");
+            }
+            int[] arr = new int[n];
 
-            while (!int.TryParse(Console.ReadLine(), out n)   || n <= 0)
+            for(int i = 0;i < n;i++)
             {
-                Console.Write("enter size of Array  integer & positive");
-            }
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
+                int x;
+                while (!int.TryParse(Console.ReadLine(), out x))
                 {
-                    if (i == j) Console.Write("1 ");
-                    else Console.Write("0 ");
+                    Console.Write("enter size of array  integer");
                 }
-                Console.WriteLine();
+                arr[i] = x;
+                sum1 += x;
+
             }
+            Console.WriteLine($"the sum of Array is : {sum1}");
             #endregion
         }
     }
