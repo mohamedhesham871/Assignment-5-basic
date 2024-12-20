34b02725d4344b14a23355f12ad4c2834142de91
20100 <mmmelkady23@gmail.com>
2024-12-20 22:20:28 +0200

    answer 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q19:Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+            Console.WriteLine("enter size of Array");
+            int n;
+
+            while (!int.TryParse(Console.ReadLine(), out n)   || n <= 0)
+            {
+                Console.Write("enter size of Array  integer & positive");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j) Console.Write("1 ");
+                    else Console.Write("0 ");
+                }
+                Console.WriteLine();
+            }
+            #endregion
         }
     }
 }
