53d1f05bbe10df4126ef1a62f2bbe647d1a4fdee
20100 <mmmelkady23@gmail.com>
2024-12-20 23:00:41 +0200

    answer 3

--- Program.cs
+++ Program.cs
@@ -22,36 +22,75 @@
             //}
             #endregion
             #region Q20:Write a program in C# Sharp to find the sum of all elements of the array.
-            int[] Arr = [10,20,30,32,41,41,352,1,345,346];
+            //int[] Arr = [10,20,30,32,41,41,352,1,345,346];
 
-            int sum = 0,sum1=0;
-            for (int i = 0; i < Arr.Length; i++)
-            {
-                sum += Arr[i];
-            }
-            Console.WriteLine($"the sum of Array is : {sum}");
+            //int sum = 0,sum1=0;
+            //for (int i = 0; i < Arr.Length; i++)
+            //{
+            //    sum += Arr[i];
+            //}
+            //Console.WriteLine($"the sum of Array is : {sum}");
 
-            // Another solve 
-            Console.WriteLine("enter size of Array");
-            int n;
-            while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
-            {
-                Console.Write("enter size of array  integer & positive");
-            }
-            int[] arr = new int[n];
+            //// Another solve 
+            //Console.WriteLine("enter size of Array");
+            //int n;
+            //while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+            //{
+            //    Console.Write("enter size of array  integer & positive");
+            //}
+            //int[] arr = new int[n];
 
-            for(int i = 0;i < n;i++)
-            {
-                int x;
-                while (!int.TryParse(Console.ReadLine(), out x))
-                {
-                    Console.Write("enter size of array  integer");
-                }
-                arr[i] = x;
-                sum1 += x;
+            //for(int i = 0;i < n;i++)
+            //{
+            //    int x;
+            //    while (!int.TryParse(Console.ReadLine(), out x))
+            //    {
+            //        Console.Write("enter size of array  integer");
+            //    }
+            //    arr[i] = x;
+            //    sum1 += x;
 
-            }
-            Console.WriteLine($"the sum of Array is : {sum1}");
+            //}
+            //Console.WriteLine($"the sum of Array is : {sum1}");
+            #endregion
+            #region Q21:Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order
+            // i will make merge sort in this example :)
+            int[] arr1 = { 1, 2, 7,15 };
+            int[] arr2 = { 4, 6, 9, 14 };
+            int [] mergedArray= new int[arr1.Length*2-1];
+            int i = 0,j=0,z=0;
+
+            //while (i<arr1.Length && j<arr2.Length)
+            //{
+            //    if (arr1[i] <= arr2[j] )
+            //    {
+            //        mergedArray[z] = arr1[i];
+            //        i++;
+            //    }
+            //    else
+            //    {
+            //        mergedArray[z] = arr2[j];
+            //        j++;
+            //    }
+            //    z++;
+            //}
+            //Console.WriteLine(string.Join(',',mergedArray));
+
+            //Another solve 
+            //for (int x=0; x<arr1.Length; x++)
+            //{
+            //    mergedArray[z] = arr1[x];
+            //    z++;
+            //}
+            //z--;
+            //for (int x = 0; x < arr2.Length; x++)
+            //{
+            //    mergedArray[z] = arr2[x];
+            //    z++;
+            //}
+            //Array.Sort(mergedArray);
+            //Console.WriteLine(string.Join(',', mergedArray));
+
             #endregion
         }
     }
