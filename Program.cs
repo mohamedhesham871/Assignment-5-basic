e0d8269319109c3bb28d49507677b1ad3bf6eab5
20100 <mmmelkady23@gmail.com>
2024-12-20 21:49:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_5_basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
